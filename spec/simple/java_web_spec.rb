73537ded3907b5ffeb971f56e0399fd74c0b4bef
michael zhang <zhangcheng@rbcon.com>
2012-05-14 01:12:55 -0700

    Add tomcat validation case for java_web
    Change-Id: I3da3b12479318c251cc9081ffb7564e8b2d6ad53
    Signed-off-by: michael zhang <zhangcheng@rbcon.com>

--- spec/simple/java_web_spec.rb
+++ spec/simple/java_web_spec.rb
@@ -1,31 +1,32 @@
 require "harness"
 require "spec_helper"
+require "nokogiri"
 
 describe BVT::Spec::Simple::JavaWeb do
 
   before(:each) do
-    @client = BVT::Harness::CFSession.new
+    @session = BVT::Harness::CFSession.new
   end
 
   after(:each) do
-    @client.cleanup!
+    @session.cleanup!
   end
 
   it "get applicatioin list", :sinatra => true, :ruby19 => true, :java_web => true do
-    app1 = @client.app("simple_app2")
+    app1 = create_app("simple_app2")
     app1.push
 
-    app2 = @client.app("tiny_java_app")
+    app2 = create_app("tiny_java_app")
     app2.push
 
-    app_list = @client.apps
+    app_list = @session.apps
     app_list.each { |app|
       app.healthy?.should be_true, "Application #{app.name} is not running"
     }
   end
 
   it "start java app with startup delay", :java_web => true do
-    app = @client.app("java_app_with_startup_delay")
+    app = create_app("java_app_with_startup_delay")
     app.push
     app.healthy?.should be_true, "Application #{app.name} is not running"
 
@@ -34,6 +35,28 @@ describe BVT::Spec::Simple::JavaWeb do
     contents.body_str.should_not == nil
     contents.body_str.should =~ /I am up and running/
     contents.close
+  end
+
+  it "tomcat validation", :java_web => true do
+    app = create_app("tomcat-version-check-app")
+    app.push
+    app.healthy?.should be_true, "Application #{app.name} is not running"
+
+    response = app.get_response(:get)
+    response.should_not == nil
+    response.response_code.should == 200
+    response.body_str.should_not == nil
+
+    doc = Nokogiri::XML(response.body_str)
+    version = doc.xpath("//version").first.content
+    version.should_not == nil
+    version.should =~ /Apache Tomcat/
+
+    packaged_version = app.manifest['tomcat_version']
+    packaged_version.should_not == nil
+    # The Tomcat version reported by the servlet is of the form
+    # 'Apache Tomcat/6.0.xx' for Tomcat 6 based releases.
+    version.split('/')[1].should == packaged_version
   end
 end
 
