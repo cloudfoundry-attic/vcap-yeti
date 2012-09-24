c47c748ce480b12e5361c4e07a515c6c8fdbcde4
Matthew M. Boedicker <matthewm@boedicker.org>
2012-09-24 16:43:48 -0700

    Add runtime debug mode testing.
    Change-Id: I229b5544e622ec49c7136e10c76467d2e2e6c7ab
    Reviewed-on: http://reviews.cloudfoundry.org/9641
    Tested-by: CI Master <cf-ci@rbcon.com>
    Reviewed-by: Ramnivas Laddad <ramnivas@ramnivas.com>
    Reviewed-by: Pin Xie <pxie@vmware.com>
    Reviewed-by: Jennifer Hickey <jhickey@vmware.com>

---
+++ spec/cfcore/debug_spec.rb
@@ -0,0 +1,17 @@
+require 'cfcore/cfcore_helper'
+
+describe 'runtime debug modes', :cfcore => true, :mcf => true do
+
+  before(:all) do
+    @session = BVT::Harness::CFSession.new
+  end
+
+  it 'should not allow debugging on cloudfoundry.com' do
+    info = @session.info
+
+    if @session.TARGET == 'cloudfoundry.com'
+      info[:allow_debug].should be_false
+    end
+  end
+
+end
