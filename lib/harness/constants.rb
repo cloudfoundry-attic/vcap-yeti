551f1bf92929156593e1e256f98d9388318da760
michael zhang <zhangcheng@rbcon.com>
2012-11-21 01:57:38 -0800

    fix the bug of default java runtime
    Change-Id: I6de26463a87565b933a2344bcc367526b4c31afb
    Signed-off-by: michael zhang <zhangcheng@rbcon.com>
    Reviewed-on: http://reviews.cloudfoundry.org/11607
    Tested-by: CI Master <cf-ci@rbcon.com>
    Reviewed-by: Pin Xie <pxie@vmware.com>

--- lib/harness/constants.rb
+++ lib/harness/constants.rb
@@ -2,7 +2,7 @@ module BVT::Harness
   ## multi versions of runtime. constant for category and runtimes.
   VCAP_BVT_INFO_RUNTIME = {
     :ruby=>['ruby19', 'ruby18'],
-    :java=>['java6', 'java7'],
+    :java=>['java', 'java7'],
     :node=>['node', 'node06', 'node08']
   }
 end
