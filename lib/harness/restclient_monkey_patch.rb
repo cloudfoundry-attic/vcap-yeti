6ef425d6b94a41473a0a19c077fd7190021ca414
michael zhang <zhangcheng@rbcon.com>
2012-12-13 02:16:46 -0800

    fix restclient monkey patch namespace
    Change-Id: Ie620f1c40e7129c9bf292d88cd4a58d06f81f0a6
    Signed-off-by: michael zhang <zhangcheng@rbcon.com>
    Reviewed-on: http://reviews.cloudfoundry.org/12424
    Tested-by: CI Master <cf-ci@rbcon.com>

--- lib/harness/restclient_monkey_patch.rb
+++ lib/harness/restclient_monkey_patch.rb
@@ -1,9 +1,12 @@
-module ResponseForException
-  def method_missing symbol, *args
-    if net_http_res.respond_to? symbol
-      net_http_res.send symbol, *args
-    else
-      super
+module RestClient
+  module ResponseForException
+    def method_missing symbol, *args
+      if net_http_res.respond_to? symbol
+        #warn "[warning] The response contained in an RestClient::Exception is now a RestClient::Response instead of a Net::HTTPResponse, please update your code"
+        net_http_res.send symbol, *args
+      else
+        super
+      end
     end
   end
 end
