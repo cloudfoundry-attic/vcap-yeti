4ed7d9ebd23fdc5baa2e8c3a89f1d1bc1b34d9ed
Jing Gong <jgong@rbcon.com>
2012-12-26 20:18:42 -0700

    fix case hang issue
    Change-Id: I801b6d879a076e62c4f15754b39b9f7f018dd470
    Signed-off-by: Jing Gong <jgong@rbcon.com>
    Reviewed-on: http://reviews.cloudfoundry.org/12878
    Tested-by: CI Master <cf-ci@rbcon.com>
    Reviewed-by: Michael Zhang <zhangcheng@rbcon.com>

--- lib/harness/restclient_monkey_patch.rb
+++ lib/harness/restclient_monkey_patch.rb
@@ -1,12 +1,6 @@
 module RestClient
   module ResponseForException
-    def method_missing symbol, *args
-      if net_http_res.respond_to? symbol
-        #warn "[warning] The response contained in an RestClient::Exception is now a RestClient::Response instead of a Net::HTTPResponse, please update your code"
-        net_http_res.send symbol, *args
-      else
-        super
-      end
+    def method_missing symbol, *arg
     end
   end
 end
