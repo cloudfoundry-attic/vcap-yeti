d51e34c7fa05d16f5d84d757ec923f5d4e528b4f
jgong <jgong@rbcon.com>
2012-12-28 15:28:20 +0800

    add cases for create/delete app/service/route
    Change-Id: I9f7febd187f435e3fe03908bb9a307987b6b5a95
    Signed-off-by: jgong <jgong@rbcon.com>
    Reviewed-on: http://reviews.cloudfoundry.org/12899
    Tested-by: CI Master <cf-ci@rbcon.com>
    Reviewed-by: Chun Yu <yuc@rbcon.com>

--- spec/space/space_spec.rb
+++ spec/space/space_spec.rb
@@ -5,12 +5,12 @@ include BVT::Spec
 
 describe BVT::Spec::OrgSpace::Space do
 
-  before(:all) do
+  before(:each) do
     @session = BVT::Harness::CFSession.new
     pending("cloud controller v1 API does not support org/space") unless @session.v2?
   end
 
-  after(:all) do
+  after(:each) do
     @session.cleanup!("all")
   end
 
@@ -31,24 +31,70 @@ describe BVT::Spec::OrgSpace::Space do
   end
 
   it "test switch space" do
-    spaces = @session.spaces
-
-    spaces.each{|s| s.delete(true) if s.name=='switch-space'}
-
-    @space = @session.space("switch-space", false)
-    @space.create
-
-    @session.select_org_and_space("","switch-space")
+    use_space("switch_space")
 
     app = create_push_app("simple_app")
 
     space = @session.current_space
-    space.name.should == "switch-space"
+    space.name.should == "switch_space"
 
     app = space.apps[0]
     app.name.should =~ /simple_app/
 
+    app.routes.each(&:delete!)
     @space.delete(true)
+  end
+
+  it "test create and delete app/service in space" do
+    use_space("new_space")
+
+    app = create_push_app("simple_app")
+    bind_service(MYSQL_MANIFEST, app)
+
+    space = @session.current_space
+
+    app = space.apps[0]
+    app.name.should =~ /simple_app/
+    service = space.service_instances[0]
+    service.name.should =~ /mysql/
+
+    app.delete!
+    space.apps.should == []
+    service.delete!
+    space.service_instances.should == []
+
+    app.routes.each(&:delete!)
+    @space.delete(true)
+  end
+
+  it "test create and delete app/route in space" do
+    use_space("new_space")
+
+    @app = create_push_app("simple_app")
+
+    space = @session.current_space
+    app = space.apps[0]
+    app.name.should =~ /simple_app/
+
+    route = app.routes[0]
+    domain = space.domains[0].name
+    route.name.should =~ /simple-app.#{domain}/
+
+    @app.delete
+    space.apps.should == []
+    @session.client.routes.should == []
+
+    @space.delete(true)
+  end
+
+  def use_space(space_name)
+    spaces = @session.spaces
+
+    spaces.each{|s| s.delete(true) if s.name==space_name}
+    @space = @session.space(space_name, false)
+    @space.create
+
+    @session.select_org_and_space("",space_name)
   end
 
 end
