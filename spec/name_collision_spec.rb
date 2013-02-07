d5c4483c0052a66e3d5a5e332f59e3dcb131b38c
Glenn Oppegard and Mark Rushakoff <pair+oppegard+mr@pivotallabs.com>
2013-02-07 10:56:15 -0800

    name collision tests

---
+++ spec/name_collision_spec.rb
@@ -0,0 +1,48 @@
+require "harness"
+require "spec_helper"
+include BVT::Spec
+
+describe "Application name collision" do
+
+  before(:all) do
+    @session = BVT::Harness::CFSession.new
+  end
+
+  after(:each) do
+    @session.cleanup!
+  end
+
+  it "will not push two apps with the same name" do
+    create_app("standalone_simple_ruby_app").push
+    expect {
+      create_app("standalone_simple_ruby_app").push
+    }.to raise_error
+  end
+
+    #it "will not push two apps with the same URL"
+  #it "will be case insensitive when not pushing two apps with the same name"
+  #it "will be case insensitive when not pushing two apps with the same URL"
+
+
+  #it "access my application root and see it's running version" do
+  #  @app = create_push_app("app_rails_version")
+  #  @app.stats.should_not == nil
+  #
+  #  runtime = @app.manifest['runtime']
+  #  version = VCAP_BVT_SYSTEM_RUNTIMES[runtime][:version].split("p",2).first
+  #  @app.get_response(:get).should_not == nil
+  #  @app.get_response(:get).to_str.should_not == nil
+  #  @app.get_response(:get).to_str.should == "running version "+version
+  #end
+  #
+  #it "precompiles assets" do
+  #  @app = create_push_app("rails_3_2_app")
+  #  @app.stats.should_not == nil
+  #  res = @app.get_response(:get, "/assets/manifest.yml")
+  #  res.should_not == nil
+  #  res.to_str.should match /application.js: application-\w/
+  #  res = @app.get_response(:get, "/assets/application.js")
+  #  res.to_str.should match /alert\(\"Hello from CoffeeScript!\"\)/
+  #end
+
+end
