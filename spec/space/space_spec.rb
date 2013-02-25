ad51118013ac9a121d4ca8eb4570d442644d1b59
Matthew Boedicker & Nate Clark <pair+matthew+nate@pivotallabs.com>
2013-02-25 11:23:27 -0800

    [fixes #44999599] Use unique space names for yetis.

--- spec/space/space_spec.rb
+++ spec/space/space_spec.rb
@@ -14,16 +14,18 @@ describe BVT::Spec::OrgSpace::Space do
     @session.cleanup!("all")
   end
 
+  let(:space_name) { "space#{rand(2**32).to_s(36)}" }
+
   it "test create space" do
     spaces = @session.spaces
 
-    spaces.each{|s| s.delete(true) if s.name=='new-space'}
-    space = @session.space("new-space", false)
+    spaces.each { |s| s.delete(true) if s.name == space_name }
+    space = @session.space(space_name, false)
     space.create
     spaces = @session.spaces
     match = false
-    spaces.each{ |s|
-      match = true if s.name == "new-space"
+    spaces.each { |s|
+      match = true if s.name == space_name
     }
     match.should == true
 
@@ -31,12 +33,12 @@ describe BVT::Spec::OrgSpace::Space do
   end
 
   it "test switch space" do
-    use_space("switch_space")
+    use_space(space_name)
 
     app = create_push_app("simple_app")
 
     space = @session.current_space
-    space.name.should == "switch_space"
+    space.name.should == space_name
 
     app = space.apps[0]
     app.name.should =~ /simple_app/
@@ -46,7 +48,7 @@ describe BVT::Spec::OrgSpace::Space do
   end
 
   it "test create and delete app/service in space" do
-    use_space("new_space")
+    use_space(space_name)
 
     create_push_app("simple_app", nil, nil, [MYSQL_MANIFEST])
 
@@ -68,7 +70,7 @@ describe BVT::Spec::OrgSpace::Space do
 
   it "test create and delete app/route in space" do
     pending "This test needs to be implemented based on correct expectations"
-    use_space("new_space")
+    use_space(space_name)
 
     @app = create_push_app("simple_app")
 
@@ -96,11 +98,11 @@ describe BVT::Spec::OrgSpace::Space do
   def use_space(space_name)
     spaces = @session.spaces
 
-    spaces.each{|s| s.delete(true) if s.name==space_name}
+    spaces.each { |s| s.delete(true) if s.name==space_name }
     @space = @session.space(space_name, false)
     @space.create
 
-    @session.select_org_and_space("",space_name)
+    @session.select_org_and_space("", space_name)
   end
 
 end
