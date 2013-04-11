373d206c013cb05bf1d594f8d46e937fc3db741c
David Stevenson and Scott Calvert <pair+stevend+scalvert@pivotallabs.com>
2013-04-11 14:21:19 -0700

    Use smaller test string for mysql service

--- lib/verification/services.rb
+++ lib/verification/services.rb
@@ -6,7 +6,8 @@ module BVT
       end
 
       def verify_service(service_vendor, app, key)
-        data = "#{rand}#{service_vendor}#{key}"
+        data = "#{rand 999999}#{service_vendor}#{key}"
+        data.length.should <= 20  # the test app has a 20 varchar limit
         app.get_response(:post, "/service/#{service_vendor}/#{key}", data)
         app.get_response(:get,  "/service/#{service_vendor}/#{key}").to_str.should == data
       end
