2bf766f1cb6d95a1b91f130a0c589ec33ffd8dc1
Nick Wade <nwade@pivotallabs.com>
2013-09-26 16:52:32 -0600

    Another attempt to make the loggregator tests more stable
    Signed-off-by: Johannes Tuchscherer <jtuchscherer@pivotallabs.com>

--- spec/tools/loggregator_spec.rb
+++ spec/tools/loggregator_spec.rb
@@ -55,9 +55,6 @@ describe "Tools::Loggregator" do
       loggregator_client.logs_for(app)
     end
 
-    # We need to restart the app after the loggregator tail is running so we can test various logs
-    app.restart
-
     begin
       Timeout.timeout(10) do
         loop { break if app.application_is_really_running? }
@@ -67,45 +64,54 @@ describe "Tools::Loggregator" do
     end
 
     matchers = {}
-    matchers[/Hello on STDOUT/] =  false
-    matchers[/Hello on STDERR/] =  false
-    matchers[/CF\[Router(\/\d)?\] STDOUT #{app.get_url}/] =  false
-    matchers[/CF\[CC(\/\d)?\] STDOUT/] =  false
-    matchers[/CF\[DEA(\/\d)?\] STDOUT/] =  false
+    matchers[/Hello on STDOUT/] = false
+    matchers[/Hello on STDERR/] = false
+    matchers[/CF\[Router(\/\d)?\] STDOUT #{app.get_url}/] = false
 
-    # Check that we get logs before we time out. If we don't, this test should fail.
-    begin
-      Timeout.timeout(10) do
-        while true
+    wait_for_matchers(app, matchers, loggregator_io, true)
+
+    app.restart
+
+    matchers = {}
+    matchers[/CF\[CC(\/\d)?\] STDOUT/] = false
+    matchers[/CF\[DEA(\/\d)?\] STDOUT/] = false
+
+    wait_for_matchers(app, matchers, loggregator_io)
+  end
+end
+
+def wait_for_matchers(app, matchers, loggregator_io, should_hit_app = false)
+  # Check that we get logs before we time out. If we don't, this test should fail.
+  begin
+    Timeout.timeout(10) do
+      while true
+        if should_hit_app
           result = app.get_response(:get)
-
           raise "Could not get response from loggregator test app" if result.code.to_i != 200
-
-          logged_output = loggregator_io.string
-
-          if logged_output =~ /Server dropped connection/
-            raise "Connection dropped! Output:\n#{logged_output}\n\nEvents:#{app.events.inspect}"
-          end
-
-          matchers.each do |matcher, matched|
-            next if matched
-            if logged_output =~ matcher
-              puts "LOGS MATCH #{matcher}"
-              matchers[matcher]=true
-            else
-              puts "LOGS DO NOT MATCH #{matcher}?"
-            end
-          end
-
-          break if matchers.values.all? { |matched| matched }
-
-          sleep(0.5)
         end
+
+        logged_output = loggregator_io.string
+
+        if logged_output =~ /Server dropped connection/
+          raise "Connection dropped! Output:\n#{logged_output}\n\nEvents:#{app.events.inspect}"
+        end
+
+        matchers.each do |matcher, matched|
+          next if matched
+          if logged_output =~ matcher
+            puts "LOGS MATCH #{matcher}"
+            matchers[matcher]=true
+          else
+            puts "LOGS DO NOT MATCH #{matcher}?"
+          end
+        end
+
+        break if matchers.values.all? { |matched| matched }
+
+        sleep(0.5)
       end
-    rescue Timeout::Error
-      raise "Did not see matching lines. Output:\n#{loggregator_io.string}\n\nEvents:#{app.events.inspect}"
-    ensure
-      Thread.kill(th)
     end
+  rescue Timeout::Error
+    raise "Did not see matching lines. Output:\n#{loggregator_io.string}\n\nEvents:#{app.events.inspect}"
   end
 end
