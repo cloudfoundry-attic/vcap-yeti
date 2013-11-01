33258d87ef5c22bf4e3d68cc8bd1652e2404a93d
Glenn Oppegard <goppegard@pivotallabs.com>
2013-11-01 11:15:06 -0600

    reduce output_volume_spec log from 100MB to 50MB

--- spec/apps/output_volume_spec.rb
+++ spec/apps/output_volume_spec.rb
@@ -7,11 +7,11 @@ describe "An app with a bunch of output", :runtime => true do
 
   with_app "dora"
 
-  it "doesn't die when printing 100MB" do
+  it "doesn't die when printing 50MB" do
     before_id = app.get_response(:get, "/id")
 
-    res = app.get_response(:get, "/logspew/104857600")
-    res.to_str.should == "Just wrote 104857600 bytes of zeros to the log"
+    res = app.get_response(:get, "/logspew/52428800")
+    res.to_str.should == "Just wrote 52428800 bytes of zeros to the log"
 
     # Give time for components (i.e. Warden) to react to the output
     # and potentially make bad decisions (like killing the app)
